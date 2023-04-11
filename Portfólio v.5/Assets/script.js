b6aa5bda4ab2aa9967443564dff5e5df500845e7
oliveeiralucas <lucas.oliveira@edu.unifil.br>
2023-04-11 00:53:26 -0300

    subindo alterações dia 11/04 (dark mode, about e menu sticky)

--- Portfólio v.5/Assets/script.js
+++ Portfólio v.5/Assets/script.js
@@ -1,24 +1,32 @@
 
-//Botão Dark Mode
+//Dark theme
 const botaoDarkMode = document.querySelector('#darkMode-icon'); //define o botão do darkMode
 
 function changeBackground() {
-  const myBody = document.querySelector('body'); 
-  const cssStyle = getComputedStyle(document.documentElement);
-    myBody.style.backgroundColor = cssStyle.getPropertyValue('--bg-color-dark-mode'); //usa a propriedade getProperty Value para pegar o valor da var
-  
+  document.documentElement.style.setProperty('--bg-color', '#0d051c');
+  //usa a propriedade getProperty Value para pegar o valor da var
   const darkModeIcon = document.querySelector('#darkMode-icon');
 
   if (darkModeIcon.classList.contains('bx-sun')) {
         darkModeIcon.classList.remove('bx-sun');
         darkModeIcon.classList.add('bx-moon');
-        myBody.style.backgroundColor = cssStyle.getPropertyValue('--bg-color');
-  } 
+        document.documentElement.style.setProperty('--bg-color', '#fdfdfd');
+        document.documentElement.style.setProperty('--text-color', '#333');
+      } 
   
   else {
         darkModeIcon.classList.remove('bx-moon');
         darkModeIcon.classList.add('bx-sun');
+        document.documentElement.style.setProperty('--bg-color', '#0d051c');
+        document.documentElement.style.setProperty('--text-color', '#fdfdfd');
   }
 }
 
 botaoDarkMode.addEventListener('click', changeBackground);
+
+
+//navbar sticky
+window.onscroll = () => {
+  let header = document.querySelector('header');
+  header.classList.toggle('sticky', window.scrollY > 100);
+};
